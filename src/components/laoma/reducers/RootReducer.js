2d56916452bb95e762611dac99ac6c388a65242c
xueqian-Destop <345589726@qq.com>
2019-04-16 00:53:33 +0800

    bugs need to display table data

--- src/components/laoma/reducers/RootReducer.js
+++ src/components/laoma/reducers/RootReducer.js
@@ -4,5 +4,5 @@ import {combineReducers} from 'redux'
 
 export const rootReducer=combineReducers({
   NumReducer,
-  UserlistReducer
+  UserList:UserlistReducer
 })
--- src/components/laoma/userlist.js
+++ src/components/laoma/userlist.js
@@ -2,16 +2,51 @@ import  React  from "react";
 import store from './store'
 import axios from 'axios';
 import {UserlistAction} from './actions/UserlistAction'
+import {Table} from 'react-bootstrap'
 export default class Userlist extends React.Component {
   constructor(){
     super()
-    this.state={}
+    this.state={
+      userlist:store.getState().UserList,
+    }
   }
-  render() {
+  componentDidMount(){
     //http://yapi.demo.qunar.com/mock/7378/api/userlist
     axios.get('http://yapi.demo.qunar.com/mock/7378/api/userlist').then(res=>{
-      store.dispatch(UserlistAction.loadUserAction(res.data.data.userlist));
+      store.dispatch(UserlistAction.loadUserAction(res.data.data.userlist))
+      console.log(store.getState().UserList)
+      this.setState({
+        userlist:store.getState().UserList,
+      })
     })
-    return <div>UserList</div>;
+    
+  }
+  render() {
+    console.log(this.state.userlist)
+    return <div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>用户名</th>
+            <th>地址</th>
+            <th>电话</th>
+            <th>备注</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {this.state.userlist.map((item,key)=>{
+            <tr key={item.Id}>
+            <td>{item.Id}</td>
+            <td>{item.UserName}</td>
+            <td>{item.Address}</td>
+            <td>{item.Phone}</td>
+            <td>{item.Remark}</td>
+          </tr>
+          })} */}
+        </tbody>
+      </Table>
+    
+    </div>
   }
 }
