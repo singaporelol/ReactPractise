118216d13eca988170be13d84c6cdfe06db325da
xueqian-Destop <345589726@qq.com>
2019-07-10 22:36:44 +0800

    redux demo

--- src/redux/count.js
+++ src/redux/count.js
@@ -1,18 +1,46 @@
 import React from 'react' 
 import { createStore } from 'redux';
+import { Button } from 'antd';
 
-export const reducerName = (state = {x:1}, action) => {
+const NumCalculator = (state = 0, action) => {
   switch (action.type) {
-    case 'ADD':
-      return state={...state,}
+    case 'ADD_NUM':
+      return state+action.payload
+    case 'MINUS_NUM':
+      return state-action.payload
     default:
       return state
   }
 }
-const store = createStore(reducerName)
+
+const store=createStore(NumCalculator);
 
 export default class Count extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      Num:0
+    }
+  }
+  AddNum=()=>{
+    store.dispatch({type:"ADD_NUM",payload:1});
+    this.setState({
+      Num:store.getState()
+    })
+  }
+  MinusNum=()=>{
+    store.dispatch({type:"MINUS_NUM",payload:1})
+    this.setState(prestate=>{
+      return{
+        Num:store.getState()
+      }
+    })
+  }
   render(){
-    return(<div>asdf</div>)
+    return(<div>
+      <p>{this.state.Num}</p>
+      <Button onClick={()=>this.AddNum()}>+1</Button>
+      <Button onClick={()=>this.MinusNum()}>-1</Button>
+    </div>)
   }
 }
